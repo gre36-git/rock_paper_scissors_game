4eebcfd41f1bf128168a73547eca714ad451bc78
gre36-git <anw51@outlook.com>
2022-07-12 21:46:15 -0500

    Silly mistake but bound to happen. Here I've writtin an initial program that I want to return a randomized selection of 'rock paper scissors' via the console. For some reason, my if else loop is always returning 'rock'. Classic assignment syntax error.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
 //javascript for rock paper scissors game
+
+
+
+var input ;
+var output;
+var computerChoice;
+
+//computer returns a randomized choice of either rock, paper, or scissors
+function randomResponse() {
+    let x = Math.floor(Math.random()*3) +1;
+    if (x = 1) {
+        return computerChoice = 'rock';
+    } else if (x = 2) {
+        return computerChoice = 'paper';
+    } else {
+        return computerChoice = 'scissors';
+    }
+    //see comment, what a doofus mistake!!
+}
+
+
+
+
+
+
